7f5f0ff5bfadba65ad61f082566fc59bcf450889
Administrator <john@gonad.org.uk>
2015-05-24 18:02:59 +0100

    base

---
+++ cookbooks/htop/recipes/default.rb
@@ -0,0 +1,16 @@
+#
+# Cookbook Name:: htop
+# Recipe:: default
+#
+
+include_recipe "yum-repoforge" if platform_family?("rhel")
+
+package "htop" do
+  version node["htop"]["version"]
+end
+
+package "iftop" do
+end
+
+package "iotop" do
+end
