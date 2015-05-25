58e15b1df729d9cf08825f2f80a8f165387339bc
Administrator <john@gonad.org.uk>
2015-05-25 22:35:49 +0100

    duply

---
+++ cookbooks/duply/README.md
@@ -0,0 +1,85 @@
+duply cookbook
+==============
+[![Build Status](https://travis-ci.org/nephilagraphic-cookbooks/duply.svg?branch=master)](https://travis-ci.org/nephilagraphic-cookbooks/duply)
+
+LWRP for Duplicity/Duply backup sets.
+
+Supports
+
+* Ubuntu 12.04
+* Ubuntu 14.04
+* Debian 7.4
+
+
+Requirements
+------------
+
+Duplicity installed.
+
+
+Usage
+-----
+
+Attributes
+----------
+
+Recipes
+-------
+
+### default
+Installs the LWRP.  Does nothing else.
+
+
+Resources
+---------
+This cookbook provides a profile resource along with direct command execution.
+
+```ruby
+duply_profile "profile_name" do
+  destination "file:///var/backups/test"
+  encrypt_for [ keys['server'][:key_id], keys['alice'][:key_id], keys['bob'][:key_id] ]
+  signed_by   keys['server'][:key_id]
+  passphrase  keys['server'][:passphrase]
+  compression :bzip2
+  volume_size 50
+  keep_full   5
+  full_every  '2M'
+  includes [
+      '/etc/duply'
+  ]
+  excludes [
+      '**.asc'
+  ]
+end
+```
+
+Execute duply commands from the cookbook
+
+```ruby
+duply "test-incr" do
+  profile "test"
+  action :incremental
+end
+```
+
+
+
+License & Authors
+-----------------
+- Author:: Ted Chen (<ted@nephilagraphic.com>)
+
+```text
+Copyright 2014, Nephila Graphic
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+```
---
+++ cookbooks/duply/providers/default.rb
@@ -0,0 +1,62 @@
+#
+# Cookbook Name:: duply
+# Provider:: profile
+#
+# Copyright (C) 2014 Nephila Graphic
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+action :nothing do
+end
+
+action :backup do
+  r = duply_command_as_user(new_resource.user,
+                            new_resource.profile, 'backup'
+  )
+  new_resource.updated_by_last_action(r.updated_by_last_action?)
+end
+
+action :full do
+  r = duply_command_as_user(new_resource.user,
+                            new_resource.profile, 'full'
+  )
+  new_resource.updated_by_last_action(r.updated_by_last_action?)
+end
+
+action :incremental do
+  r = duply_command_as_user(new_resource.user,
+                            new_resource.profile, 'incr'
+  )
+  new_resource.updated_by_last_action(r.updated_by_last_action?)
+end
+
+action :restore do
+  r = duply_command_as_user(new_resource.user,
+                            new_resource.profile, 'restore', [new_resource.destination]
+  )
+  new_resource.updated_by_last_action(r.updated_by_last_action?)
+end
+
+private
+
+def duply_command_as_user(user, profile, command, args = [])
+  # We actually use su command as root to fully run as the specified user
+  # along with all its environment variables, etc.  This is required so that pre and post
+  # are in the right environment.
+  execute "duply_#{profile}_#{command}" do
+    command "su - #{user} -c \"/usr/bin/flock -n /var/run/duply.lock /usr/bin/duply #{profile} #{command} #{args.flatten.join ' '}\";sleep 1"
+    user 'root'
+    group 'root'
+  end
+end
